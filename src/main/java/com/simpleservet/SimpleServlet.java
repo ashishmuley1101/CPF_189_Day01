98487b0acd5a4a09f71bdb6bc7bdf703501eb311
ashishmuley1101 <ashishmuley1101@gmail.com>
2022-09-17 19:25:53 +0530

    MyFirstServlet Initilize

---
+++ src/main/java/com/simpleservet/SimpleServlet.java
@@ -0,0 +1,21 @@
+package com.simpleservet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/Hello")
+public class SimpleServlet extends HttpServlet {
+
+        protected void doGet(HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
+            response.setContentType("text/html");
+            PrintWriter out = response.getWriter();
+            out.println("<h3> Hello world ashish first servlet</h3>");
+            out.close();
+        }
+
+    }
---
+++ target/MyFirstServlet/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Welcome MyFirstServlet </h2>
+</body>
+</html>
